c0a2c2b19953883f8d0d6d19c18ef39a0b254227
stefanyyason <stefanyyason_18@kharisma.ac.id>
2020-03-24 16:53:18 +0800

    Update bio
    Hello world berubah menjadi nama, jurusan, dan nim

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Stefany 51017018 Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)
